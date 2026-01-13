07fa068e9dec623702fba15d00cb841a706d022d
ryanrmills <ryanroymills@gmail.com>
2026-01-13 14:48:12 -0800

    livecode

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,10 @@
 public class Node {
-    
+    public Node next;
+    public char value;
+
+    public Node(char value, Node next){
+        this.value = value;
+        this.next = next;
+
+    }
 }
